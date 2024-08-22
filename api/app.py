816d027809e22c1b1bd02f6503804fa8ebd4f451
K Srirama Srikar <143777264+k-srirama-srikar@users.noreply.github.com>
2024-08-22 22:52:34 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,21 @@ app = Flask(__name__)
 @app.route("/")
 def temp():
     return render_template("page.html")
+
+
+@app.route("/check", methods=["GET", "POST"])
+def check():
+    c1 = False
+    ans1 = ""
+
+    if request.method == "POST":
+        pwd = request.form['pwd']
+        if pwd == "aurjaoobt":
+            c1 = True
+            ans1 = "first link"
+        else:
+            return render_template("page.html")
+    return render_template("page.html", c1=c1, ans1=ans1)
 
 
 if __name__ == '__main__':
